47318f91c9c689a335a42ac2efe2315b59f81957
Hamdy Shabeeb <34157559+Hamdyshabeeb@users.noreply.github.com>
2024-04-23 23:57:02 +0200

    create Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,44 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { globalContext } from '../context/GlobalContext';
+
+export default function Header() {
+	const { search, setSearch, handelFormSubmit } = useContext(globalContext);
+
+	return (
+		<header>
+			<h2>
+				<Link to="/">Recipes</Link>
+			</h2>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					console.log('submited');
+					handelFormSubmit(`https://forkify-api.herokuapp.com/api/search?q=${search}
+
+
+`);
+				}}
+			>
+				<input
+					type="text"
+					placeholder="Search recipes"
+					aria-label="search recipes"
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+				<button type="submit">Search</button>
+			</form>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home </Link>
+					</li>
+					<li>
+						<Link to="/favorites">Favorites </Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
